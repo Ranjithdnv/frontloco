792815910121d37456f7d3c9c86057db0a5eb932
ranjith <rith8596@gmail.com>
2024-03-02 13:27:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const GeolocationTracing = () => {
+  const [location, setLocation] = useState(null);
+  const [longi, setlongi] = useState(null);
+  const [longit, setlongit] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const successHandler = (position) => {
+      const { latitude, longitude } = position.coords;
+      setLocation({ latitude, longitude });
+      const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+      console.log(`https://www.google.com/maps?q=${latitude},${longitude}`);
+    };
+
+    const errorHandler = (err) => {
+      setError(err.message);
+    };
+
+    // Check if geolocation is supported
+    if ("geolocation" in navigator) {
+      // Get the current position
+      navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
+    } else {
+      setError("Geolocation is not supported in this browser");
+    }
+
+    const ffffff = async (position) => {
+      await axios.get(" http://localhost:3003/").then((res) => {
+        console.log(res.data);
+      }); //
+    };
+    ffffff();
+  }, []);
+
+  const ffff = (position) => {
+    window.open(
+      `https://www.google.com/maps?q=${location.latitude},${location.longitude}`
+    );
+  };
+  const fffff = async (position) => {
+    await axios
+      .post(" http://localhost:3003/", {
+        moviename: 2,
+        writtenby: 1,
+      })
+      .then((res) => {
+        console.log(res.data.moviename);
+        console.log(res);
+      }); //
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {location ? (
+        <div>
+          <h2>Your current location:</h2>
+          <p>Latitude: {location.latitude}</p>
+          <p>Longitude: {location.longitude}</p>
+        </div>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <>
+        <button onClick={ffff}>vk</button>
+      </>
+      <>
+        <button onClick={fffff}>vk</button>
+      </>
     </div>
   );
-}
+};
 
-export default App;
+export default GeolocationTracing;
